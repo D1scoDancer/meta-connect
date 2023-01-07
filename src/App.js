c1d81e1dda737eca74d9f24169a906c8fec73896
D1scoDancer <apshulikov@edu.hse.ru>
2023-01-07 11:06:59 +0300

    WalletCardEthers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css'
 
 import WalletCard from './WalletCard'
+import WalletCardEthers from './WalletCardEthers'
 
 function App() {
     return (
         <div className="App">
             <WalletCard />
+            <WalletCardEthers />
         </div>
     )
 }
